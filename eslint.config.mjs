65a7ecc4f8ad4d4c363f2ac8c5344a815bef62c5
bojidaryovchev <blizzardcon10@gmail.com>
2025-09-30 15:22:18 +0300

    chore: ignore next env d ts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignores: [".next/**", ".sst/**"],
+    ignores: [".next/**", "next-env.d.ts", ".sst/**"],
   },
 ];
 
